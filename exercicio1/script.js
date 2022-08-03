ca59811b24e515b5eed08f693eff922f2cd21ecb
rodolfofreitass <rodolfofreitas38@hotmail.com>
2022-08-02 23:39:43 -0300

    execucao da atividade

---
+++ exercicio1/script.js
@@ -0,0 +1,19 @@
+const sacolao = []
+const bananaPrata = {
+    nome: "Banana Prata",
+    preco: 5.19,
+    disponibilidade: true,
+}
+const maca = {
+    nome: "Maca",
+    preco: 6.19,
+    disponibilidade: true,
+}
+const cebola = {
+    nome: "Cebola",
+    preco: 4.59,
+    disponibilidade: false,
+}
+
+sacolao.push(bananaPrata,maca,cebola)
+console.log(sacolao)
